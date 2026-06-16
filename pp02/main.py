4f5667d0cf13d3b839a51d7af3a71b68047a0d1e
Maria <mashasarukhan@gmail.com>
2026-06-16 22:21:30 +0300

    practica02

--- pp02/main.py
+++ pp02/main.py
@@ -62,4 +62,3 @@ if __name__ == "__main__":
     widget = Widget1()
     widget.show()
     app.exec()
-
--- pp02/table.py
+++ pp02/table.py
@@ -1,7 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QMessageBox, QInputDialog
+from PyQt5.QtCore import Qt
 from podcl import Connect
 
 # Important:
@@ -16,19 +17,27 @@ class Widget2(QWidget):
         self.ui = Ui_Widget2()
         self.ui.setupUi(self)
         self.connect=Connect()
+        self.table = "oborudovanie"
         self.insert()
         self.ui.tabspis.clicked.connect(self.tabspis)
         self.ui.tabmesta.clicked.connect(self.tabmesta)
         self.ui.tabob.clicked.connect(self.tabob)
         self.ui.zap2.clicked.connect(self.zap2)
         self.ui.zap3.clicked.connect(self.zap3)
+        self.ui.add.clicked.connect(self.add)
+        self.ui.izm.clicked.connect(self.izm)
+        self.ui.spisat.clicked.connect(self.spisat)
         self.ui.exit.clicked.connect(self.exit)
 
     def insert(self):
+        self.table = "oborudovanie"
         self.connect.cur.execute('SELECT * FROM public.oborudovanie')
         headers = [desc[0] for desc in self.connect.cur.description]
         self.ui.tableWidget.setHorizontalHeaderLabels(headers)
         self.data=self.connect.cur.fetchall()
+        self.ui.spisat.setEnabled(True)
+        self.ui.add.setEnabled(True)
+        self.ui.izm.setEnabled(True)
         #self.ui.tableWidget.setRowCount(len(self.data))
         #self.ui.tableWidget.setColumnCount(len(self.data[0]))
         self.ui.tableWidget.setRowCount(len(self.data))
@@ -39,11 +48,15 @@ class Widget2(QWidget):
                 item=QTableWidgetItem(str(cell_data))
                 self.ui.tableWidget.setItem(row_num, col_num, item)
                 self.ui.tabob.setEnabled(False)
+                if col_num == 0:
+                    item.setFlags(item.flags() & ~Qt.ItemIsEditable)
+                    self.ui.tableWidget.setItem(row_num, col_num, item)
 
     def tabob(self):
         self.insert()
         self.ui.tabspis.setEnabled(True)
         self.ui.tabmesta.setEnabled(True)
+        self.ui.spisat.setEnabled(True)
 
     def tabspis(self):
         self.connect.cur.execute('SELECT * from spisannoe_oborudovanie')
@@ -62,8 +75,12 @@ class Widget2(QWidget):
                 self.ui.tabob.setEnabled(True)
                 self.ui.tabmesta.setEnabled(True)
                 self.ui.tabspis.setEnabled(False)
+                self.ui.spisat.setEnabled(False)
+                self.ui.add.setEnabled(False)
+                self.ui.izm.setEnabled(False)
 
     def tabmesta(self):
+        self.table = "rabochie_mesta"
         self.connect.cur.execute('SELECT * from rabochie_mesta')
         headers3 = [desc[0] for desc in self.connect.cur.description]
         self.ui.tableWidget.setHorizontalHeaderLabels(headers3)
@@ -80,6 +97,12 @@ class Widget2(QWidget):
                 self.ui.tabob.setEnabled(True)
                 self.ui.tabspis.setEnabled(True)
                 self.ui.tabmesta.setEnabled(False)
+                self.ui.spisat.setEnabled(False)
+                if col3_num == 0:
+                    item.setFlags(item.flags() & ~Qt.ItemIsEditable)
+                    self.ui.tableWidget.setItem(row3_num, col3_num, item)
+                    self.ui.add.setEnabled(True)
+                    self.ui.izm.setEnabled(True)
 
     def zap2(self):
         self.connect.cur.execute('SELECT sotrudniki.full_name AS "Сотрудник", COUNT(oborudovanie.unical_nomer) AS "Количество закрепленных устройств" FROM sotrudniki JOIN rabochie_mesta ON sotrudniki.id_sotrudnika = rabochie_mesta.id_sotrudnika JOIN oborudovanie ON rabochie_mesta.id_mesta = oborudovanie.id_mesta GROUP BY sotrudniki.full_name HAVING COUNT(oborudovanie.unical_nomer) > 3 ORDER BY "Количество закрепленных устройств" DESC')
@@ -109,6 +132,61 @@ class Widget2(QWidget):
                 self.ui.zap2.setEnabled(True)
                 self.ui.zap3.setEnabled(False)
 
+    def add(self):
+        row = self.ui.tableWidget.rowCount()
+        self.ui.tableWidget.insertRow(row)
+
+    def izm(self):
+        if self.table=='oborudovanie':
+            row=self.ui.tableWidget.currentRow()
+            nomer=self.ui.tableWidget.item(row, 0).text()
+            tip=self.ui.tableWidget.item(row, 1).text()
+            god=self.ui.tableWidget.item(row, 2).text()
+            mesto=self.ui.tableWidget.item(row, 3).text()
+            self.connect.cur.execute(f"SELECT * from oborudovanie where unical_nomer='{nomer}'")
+            str=self.connect.cur.fetchone()
+            if str:
+                self.connect.cur.execute(f"UPDATE oborudovanie set id_tipa='{tip}', god_vipuska='{god}', id_mesta='{mesto}' where unical_nomer='{nomer}'")
+                self.connect.con.commit()
+                QMessageBox.information(self, 'Сообщение', 'Запись обновлена')
+                self.insert()
+            else:
+                self.connect.cur.execute(f"INSERT INTO oborudovanie (unical_nomer, id_tipa, god_vipuska, id_mesta) VALUES ('{nomer}', '{tip}', '{god}', '{mesto}')")
+                self.connect.con.commit()
+                QMessageBox.information(self, 'Сообщение', 'Запись добавлена')
+                self.insert()
+
+        elif self.table == 'rabochie_mesta':
+            self.izm_mesta()
+
+    def izm_mesta(self):
+        row = self.ui.tableWidget.currentRow()
+        id_mesta=self.ui.tableWidget.item(row, 0).text()
+        id_cabineta=self.ui.tableWidget.item(row, 1).text()
+        id_sotrudnika=self.ui.tableWidget.item(row, 2).text()
+        self.connect.cur.execute(f"SELECT * from rabochie_mesta where id_mesta='{id_mesta}'")
+        str2=self.connect.cur.fetchone()
+        if str2:
+            self.connect.cur.execute(f"UPDATE rabochie_mesta set id_cabineta='{id_cabineta}', id_sotrudnika='{id_sotrudnika}' where id_mesta='{id_mesta}'")
+            self.connect.con.commit()
+            QMessageBox.information(self, 'Сообщение', 'Запись обновлена')
+            self.tabmesta()
+        else:
+            self.connect.cur.execute(f"INSERT INTO rabochie_mesta (id_mesta, id_cabineta, id_sotrudnika) VALUES ('{id_mesta}', '{id_cabineta}', '{id_sotrudnika}')")
+            self.connect.con.commit()
+            QMessageBox.information(self, 'Сообщение', 'Запись добавлена')
+            self.tabmesta()
+
+    def spisat(self):
+        row = self.ui.tableWidget.currentRow()
+        nomer = self.ui.tableWidget.item(row, 0).text()
+        prichina, ok = QInputDialog.getText(self, 'Списания', 'Причина списания:')
+        if not ok or not prichina:
+            return
+        self.connect.cur.execute(f"CALL spisanie_oborudovaniya('{nomer}', '{prichina}')")
+        self.connect.con.commit()
+        QMessageBox.information(self, 'Сообщение', 'Оборудование списано')
+        self.insert()
 
     def exit(self):
         self.hide()
